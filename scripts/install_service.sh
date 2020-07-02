f3a6fcb51ba2e082b0334b855b6073239e373f04
Adrian Vladu <avladu@cloudbasesolutions.com>
2020-07-02 15:02:25 +0300

    Remove systemd service before install

--- scripts/install_service.sh
+++ scripts/install_service.sh
@@ -9,6 +9,9 @@ BIN_PATH="/usr/local/bin/openstack-networkd.sh"
 SRC_SERVICE_PATH="${BASEDIR}/../systemd/${SERVICE_NAME}.service"
 SERVICE_PATH="/etc/systemd/system/${SERVICE_NAME}.service"
 
+systemctl stop "${SERVICE_NAME}" || true
+systemctl disable "${SERVICE_NAME}" || true
+
 mkdir -p "/var/lib/openstack-networkd"
 
 cp -f "${SRC_BIN_PATH}" "${BIN_PATH}"
