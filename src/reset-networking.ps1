082ba4cec5860c05ddf5d78d55cfa4c62537218f
Adrian Vladu <avladu@cloudbasesolutions.com>
2023-10-04 18:06:51 +0300

    Update reset-networking.ps1

--- src/reset-networking.ps1
+++ src/reset-networking.ps1
@@ -1,5 +1,6 @@
 #ps1
 
+# Cleaning the ghost / hidden network devices
 $action = {
     (Get-PnpDevice -Class NET | Where-Object {$_.Status -eq "Unknown"}).InstanceId | ForEach-Object {
         $instanceRegKeyPath = "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_)"
@@ -14,3 +15,12 @@ $action = {
 
 # This will work only if the detach is done before the attach
 Register-WmiEvent -Query "SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance isa 'Win32_NetworkAdapter'" -Action $action
+
+# Best way / safest way is to use ipconfig /release and /renew at boot time
+# If running as LocalScript or Userdata using cloudbase-init
+
+# ps1
+ipconfig /release
+ipconfig /renew
+# 1002 - don’t reboot now and run the plugin again on next boot
+exit 1002
