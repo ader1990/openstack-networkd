0b565250376990bcc299e99e5164f5b75f9d1ce3
Adrian Vladu <avladu@cloudbasesolutions.com>
2023-10-04 17:35:54 +0300

    Trigger a cleanup of old network adapters

--- src/reset-networking.ps1
+++ src/reset-networking.ps1
@@ -1,9 +1,12 @@
 #ps1
 
 $action = {
-    Remove-ItemProperty -Name "NetworkConfigPlugin" -Path 'HKLM:\SOFTWARE\Cloudbase Solutions\Cloudbase-Init\*\Plugins';
-    & 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\Python\Scripts\cloudbase-init.exe' --config-file 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\ipchange\conf\cloudbase-init.conf'
-    & 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\Python\python.exe' 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\ipchange\scripts\notify.py' "NIC_ADD"
+    Get-PnpDevice -Class NET | Where-Object {$_.Status -eq "Unknown"}).InstanceId | ForEach-Object {
+        $instanceRegKeyPath = "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_)"
+        Get-Item $instanceRegKeyPath | Select-Object -ExpandProperty Property | ForEach-Object {
+            Remove-ItemProperty -Path $instanceRegKeyPath -Name $_
+        }
+    }
 }
 
 Register-WmiEvent -Query "SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance isa 'Win32_NetworkAdapter'" -Action $action
